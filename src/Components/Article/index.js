b256e57794476eee1f0590f188a4aac0121335eb
Jacky Zhao <liang.zhao83@gmail.com>
2018-10-28 01:32:18 -0700

    working on blogs

---
+++ src/Components/Article/index.js
@@ -0,0 +1,213 @@
+import React, { Component } from 'react'
+import {
+    Grid, Button
+} from '@material-ui/core'
+import { orange, red } from '@material-ui/core/colors'
+import { compose } from 'recompose'
+import { withStyles } from '@material-ui/core/styles'
+import { withContext } from '../../context'
+
+const styles = theme => ({
+    // generic css
+    detailButton: {
+        color: '#fff',
+        backgroundColor: orange[500],
+        '&:hover': {
+            backgroundColor: orange[800],
+          },
+        fontSize: '1rem',
+        fontWeight: '900',
+    },
+    likeButton: {
+        color: '#fff',
+        backgroundColor: red[500],
+        '&:hover': {
+            backgroundColor: red[800],
+          },
+        fontSize: '1rem',
+        fontWeight: '900',
+    },
+    // single col css
+    singleColImage: {
+        marginBottom: 25,
+        position: 'relative',
+    },
+    singleColTitle: {
+        // fontSize: '1.3rem',
+        fontWeight: '900',
+        color: '#fff',
+        textShadow: `2px 2px 2px #222`,
+    },
+    singleColTitleShadow: {
+        position: 'absolute',
+        top: '30%',
+        background: 'transparent',
+        width: '100%',
+        padding: 20,
+    },
+    singleColButtonShadow: {
+        position: 'absolute',
+        bottom: 0,
+        background: 'transparent',
+        width: '100%',
+        padding: 20,
+    },
+    // multiple col css
+    multipleColImage: {
+        marginBottom: 5,
+        position: 'relative',
+        "&:hover $multipleColShadow": {
+            visibility: 'visible',
+        },
+    },
+    multipleColShadow: {
+        position: 'absolute',
+        top: 0,
+        background: 'transparent',
+        width: '100%',
+        padding: 20,
+        visibility: 'hidden',
+    },
+    multipleColTitle: {
+        fontSize: '1.1rem',
+        fontWeight: '900',
+        color: theme.palette.primary.main,
+        letterSpacing: .7,
+        cursor: 'pointer',
+    },
+    multipleColSummary: {
+        overflowWrap: 'break-word',
+        fontSize: '.75rem',
+        fontWeight: '400',
+        lineHeight: 1.5,
+        color: '#000000',
+        letterSpacing: .9,
+        marginTop: 5,
+        paddingBottom: 10,
+        borderBottom: `1px solid #ccc`,
+        cursor: 'pointer',
+    },
+    multipleColInfo: {
+        fontSize: '.6rem',
+        fontWeight: '800',
+        marginTop: 5,
+        color: theme.palette.primary.main,
+    }
+})
+
+  
+class Article extends Component {
+
+    /* 
+        < 650 : 1 col 
+        < 1280 : 2 col
+        < 1650 : 3 col 
+
+        1. 'windowWidth' would match material-ui's breakpoints, however, due to the fact that drawer would display after 960, the viewWidth is actually used when calculating item's width and height
+        2. we will use localStorage to pass product info to detail page, the key is 'amzfunstuff-{productId}', and the value is stringified product info
+    */
+   createArticles = (classes, article, viewWidth, windowWidth, liked, addLiked) => {
+
+        if(viewWidth < 650){
+            return this.createSingleColArticle(classes, article, viewWidth)
+        }else if(windowWidth < 1280){
+            return this.createMultipleColArticle(classes, article, viewWidth, liked, addLiked, 2)
+        }else if(windowWidth < 1650){
+            return this.createMultipleColArticle(classes, article, viewWidth, liked, addLiked, 2)
+        }else{
+            return this.createMultipleColArticle(classes, article, viewWidth, liked, addLiked, 3)
+        }
+    }
+
+    createSingleColArticle = (classes, article, viewWidth) => {
+
+        const viewWidthRatio = 0.85
+        const heightToWidthRatio = 350/525
+        const maxWidth = 525
+        
+        const adjustedWidth = viewWidth * viewWidthRatio > maxWidth ? maxWidth : viewWidth * viewWidthRatio
+        const adjustedHeight = adjustedWidth * heightToWidthRatio
+
+        const adjustedFontSize = viewWidth < 350 ? '1rem' : '1.3rem'
+
+        return <Grid item>
+                    <Grid container justify="center">
+                        <div className={classes.singleColImage}>
+                            <a href={article.link} rel="nofollow" target="_blank">
+                                <img src={`/articles/images/${article.image}.jpg`} alt={article.title} style={{ width:adjustedWidth, height:adjustedHeight }}/>
+                                <Grid container justify="center" className={classes.singleColTitleShadow}>
+                                    <div className={classes.singleColTitle} style={{ fontSize:adjustedFontSize }}>{article.title}</div>
+                                </Grid>
+                                <Grid container justify="flex-end" className={classes.singleColButtonShadow}>
+                                    <Button variant="contained" className={classes.detailButton} onClick={() => { this.props.navToLink(article.link, true) }}>查看详情</Button>
+                                </Grid>
+                            </a>
+                        </div>
+                    </Grid>
+                </Grid>
+                
+    }
+
+    createMultipleColArticle = (classes, article, viewWidth, liked, addLiked, column) => {
+
+        const viewWidthRatio = 0.8
+        const heightToWidthRatio = 350/525
+        const maxWidth = 525
+        
+        const adjustedWidth = (viewWidth * viewWidthRatio)/column > maxWidth ? maxWidth : (viewWidth * viewWidthRatio)/column
+        const adjustedHeight = adjustedWidth * heightToWidthRatio
+
+        const likeOrNot = liked != null && liked.has('b-'+article.id) ?
+                             <div className={classes.multipleColLikes}><span style={{fontSize:'10px', color:'red'}}><i className="fas fa-heart"></i></span> {article.likes+1}</div> :
+                             <div className={classes.multipleColLikes}><span style={{fontSize:'10px'}}><i className="far fa-heart"></i></span> {article.likes}</div>
+
+        return <Grid item style={{ marginBottom: 20 }}>
+                    <Grid container justify="center">
+                        <div className={classes.multipleColImage}>
+                            <a href={article.link} rel="nofollow" target="_blank">
+                                <img src={`/articles/images/${article.image}.jpg`} alt={article.title} style={{ width:adjustedWidth, height:adjustedHeight }}/>
+                                <Grid container justify="flex-end" className={classes.multipleColShadow}>
+                                    <Button variant="contained" className={classes.likeButton} onClick={(event) => { event.preventDefault(), addLiked('b-'+article.id) }}>喜欢</Button>
+                                </Grid>
+                            </a>
+                        </div>
+                        <Grid container direction="column" justify="center" alignItems="center" style={{ width:adjustedWidth }}>
+                            <div className={classes.multipleColTitle} onClick={() => {this.props.navToLink(`/blogs/${article.link}`, true)}}>
+                                {article.title}
+                            </div>
+                            <div className={classes.multipleColSummary} style={{ width:adjustedWidth - 20 }}>
+                                {article.summary}
+                            </div>
+                            <div className={classes.multipleColInfo} style={{ width:adjustedWidth - 20 }}>
+                                <Grid container justify="space-around" alignItems="center">
+                                    <Grid item>
+                                        {likeOrNot}
+                                    </Grid>
+                                    <Grid item>
+                                        <div>{article.category}</div>
+                                    </Grid>
+                                </Grid>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Grid>
+    }
+
+    render() {
+
+        const { classes, article, viewWidth, windowWidth, liked, addLiked } = this.props
+        
+        const content = this.createArticles(classes, article, viewWidth, windowWidth, liked, addLiked)
+
+        return <Grid item xs={12} sm={6} md={6} lg={6} xl={4}>
+                    <Grid container justify="center">
+                        {content}
+                    </Grid>  
+                </Grid>
+    }
+}
+
+export default compose(
+    withContext,
+    withStyles(styles)
+)(Article)
--- src/Components/Articles/index.js
+++ src/Components/Articles/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import {
-    Grid, Hidden, FormControl, Select, MenuItem, CircularProgress
+    Grid, CircularProgress
 } from '@material-ui/core'
-import { Slider } from 'material-ui-slider'
-import Product from '../Product'
+import Article from '../Article'
 import { compose } from 'recompose'
 import { withStyles } from '@material-ui/core/styles'
 import axios from "axios"
@@ -12,6 +11,13 @@ import axios from "axios"
 const styles = theme => ({
     root: {
         flexGrow: 1,
+        marginLeft: '5%',
+        marginRight: '5%',
+    },
+    banner: {
+        width: '100%',
+        height: 'auto',
+        marginBottom: 20,
     },
 })
 
@@ -19,23 +25,116 @@ const styles = theme => ({
 class Articles extends Component {
 
     state = {
-
+        viewWidth: window.innerWidth >= 960 ? window.innerWidth - 240 : window.innerWidth,
+        articles: [],
+        index: null,
+        liked: null,
     }
 
     componentDidMount() {
+        window.addEventListener("resize", this.handleResize)
+        window.addEventListener('scroll', this.handleScroll)
 
+        this.loadLiked()
+
+        const { location: { pathname } } = this.props
+
+        if(pathname != null & pathname == '/blogs'){
+            this.loadArticles('blogs')
+        }else{
+            this.loadArticles('guides')
+        }
     }
 
     componentWillUnmount(){
+        window.removeEventListener('resize', this.handleResize)
+        window.removeEventListener('scroll', this.handleScroll)
     }
 
+    // load liked set
+    loadLiked = () => {
+        const liked = new Set(JSON.parse(localStorage.getItem(`amzfunstuff-articles-liked`)))
+        this.setState({ liked })
+    }
 
+    // add like into liked
+    addLiked = (articleId) => {
+        const liked = this.state.liked
+        liked.add(articleId)
+        this.setState({ liked })
+        localStorage.setItem('amzfunstuff-articles-liked', JSON.stringify(Array.from(liked)))
+    }
+
+    // load blogs (load once for all) 
+    loadArticles = async (types) => {
+        // load blogs/guides 
+        let articlesURL = null
+        if(types == 'blogs'){
+            articlesURL = `/articles/blogs`
+        }else{
+            articlesURL = `/articles/guides`
+        } 
+
+        let next = true
+        let counter = 1
+        let articles = []
+        while(next){
+            const content = await axios.get(`${articlesURL}/${counter}.json`)
+            articles = content.data.articles.concat(articles)
+            counter++
+            next = content.data.next
+        }
+        const index = 24
+        this.setState({ articles, index })
+    }
+
+    // handle scroll - load more by modifying index
+    handleScroll = () => {
+        // if(innerHeight + scrollY >= document.body.offsetHeight - 50) - this worked added 100% height to html and body tags, and the new solution was to use document.getElementById('root').offsetHeight (with 'root' being a container that actually occupied 100% of the page height)
+
+        const scrollTop = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop)
+
+        if(innerHeight + scrollTop >= document.getElementById('root').offsetHeight - 50){
+            const index = this.state.index + 12 > this.state.articles.length ? this.state.articles.length : this.state.index + 12
+            this.setState({ index })
+        }
+    }
+
+    // handle resize - pass viewWidth to each Article sub-component for better responsive design
+    handleResize = () => {
+        this.setState({ viewWidth: window.innerWidth >= 960 ? window.innerWidth - 240 : window.innerWidth })
+    }
 
     render() { 
         const { classes } = this.props
-        
+        const { articles, index, viewWidth, liked } = this.state
+
+        //create banner sub-component
+        const banner =  <a href="www.amazon.com" rel="nofollow" target="_blank">
+                            <img className={classes.banner} src={`/articles/images/banner.png`}/>
+                        </a>
+                        
+        // create content sub-component
+        let content = null
+        if(articles.length == 0){
+            content = <Grid container justify="center" alignItems="center" style={{ height: window.innerHeight * .6 }} ><CircularProgress size={100} /></Grid>
+        }else{
+            const finalIndex = index > articles.length ? articles.length : index
+            const finalArticle = articles.slice(0, finalIndex)
+
+            content = <Grid container justify="center">
+                        {finalArticle.map(article => (
+                            <Article key={article.id} article={article} windowWidth={window.innerWidth} viewWidth={viewWidth} liked={liked} addLiked={this.addLiked}/>
+                        ))}
+                    </Grid>
+        }
+
+        // console.log('window.innerWidth ', window.innerWidth)
+        // console.log('viewWidth ', viewWidth)
+
         return <div className={classes.root}>
-                    blogs
+                    {banner}
+                    {content}
                 </div>
     }
 }
