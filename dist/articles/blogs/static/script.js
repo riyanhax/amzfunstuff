1d09c640dbd252e37553278178cf25ffb640de8b
Jacky Zhao <liang.zhao83@gmail.com>
2018-10-30 00:10:19 -0700

    build

--- dist/articles/blogs/static/script.js
+++ dist/articles/blogs/static/script.js
@@ -2,10 +2,6 @@ jQuery(function($) {
     var $bodyEl = $('body'),
         $sidedrawerEl = $('#sidedrawer');
     
-    
-    // ==========================================================================
-    // Toggle Sidedrawer
-    // ==========================================================================
     function showSidedrawer() {
       // show overlay
       var options = {
@@ -25,27 +21,12 @@ jQuery(function($) {
       }, 20);
     }
     
-    
     function hideSidedrawer() {
       $bodyEl.toggleClass('hide-sidedrawer');
     }
     
-    
     $('.js-show-sidedrawer').on('click', showSidedrawer);
     $('.js-hide-sidedrawer').on('click', hideSidedrawer);
-    
-    
-    // ==========================================================================
-    // Animate menu
-    // ==========================================================================
-    var $titleEls = $('strong', $sidedrawerEl);
-    
-    $titleEls
-      .next()
-      .hide();
-    
-    $titleEls.on('click', function() {
-      $(this).next().slideToggle(200);
-    });
+
   });
   
