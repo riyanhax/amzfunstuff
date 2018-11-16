44de11032aff964d4347f23775ac4fcfe5b18be7
Jacky Zhao <liang.zhao83@gmail.com>
2018-11-15 23:06:29 -0800

    working on data

--- Makefile
+++ Makefile
@@ -3,3 +3,10 @@ productsProcess:
 
 uploadSandbox:
 	aws s3 sync ./dist s3://amzfunstuff --acl public-read --delete
+
+### products extraction
+
+# thisiswhyimbroke
+
+retrieve-thisiswhyimbroke:
+	node ./utils/products/thisiswhyimbroke/retrieve.js 
---
+++ utils/products/thisiswhyimbroke/retrieve.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const axios = require('axios') 
+const fs = require('fs')
+
+const filepath = __dirname + '/data.json'
+
+// date to stop search (start date of last time)
+const dateToStop = '2018-11-11T09:02:00.000Z'
+
+const main = async () => {
+
+    const productsURL = 'https://www.thisiswhyimbroke.com/api/lists/new'
+    let counter = 1
+    let next = true
+    let products = []
+
+    while(next){
+        const content = await axios.get(`${productsURL}/${counter}`)
+        const posts = content.data.posts
+
+        // iterate posts to create product
+        for(const post of posts){
+
+            if(post.published == dateToStop){
+                next = false
+            }
+
+            if(post.link.indexOf('amazon') == -1){
+                continue
+            }
+
+            let product = {
+                id: post.link.split('dp/')[1].split('/')[0],
+                titleCN: '',
+                titleEN: post.title,
+                description: '',
+                content: post.content,
+                exId: post.id,
+                price: post.price,
+                likes: post.saves,
+                link: post.link,
+                uri: `https://www.thisiswhyimbroke.com${post.uri}`,
+                source: 'thisiswhyimbroke'
+            }
+            products.push(product)
+        }
+
+        counter++
+    }
+
+    const json = JSON.stringify(products, undefined, 2)
+    fs.writeFileSync(filepath, json, 'utf8')  
+}
+
+main()
