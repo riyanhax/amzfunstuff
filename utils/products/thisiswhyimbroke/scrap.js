ee9fb62a21570f67c83d963ff3910432789e8b0e
Jacky Zhao <liang.zhao83@gmail.com>
2018-11-16 22:32:55 -0800

    differentiate different platforms

--- utils/products/thisiswhyimbroke/scrap.js
+++ utils/products/thisiswhyimbroke/scrap.js
@@ -2,18 +2,26 @@
 
 const axios = require('axios') 
 const fs = require('fs')
+const shortid = require('shortid')
 
-const filepath = __dirname + '/data/data.json'
+const amazonfilepath = __dirname + '/data/amazon.json'
+const etsyfilepath = __dirname + '/data/etsy.json'
+const kickstarterfilepath = __dirname + '/data/kickstarter.json'
+const othersfilepath = __dirname + '/data/others.json'
 
 // date to stop search (start date of last time)
-const dateToStop = '2018-11-11T09:02:00.000Z'
+const dateToStop = '2018-11-04T09:05:00.000Z'
 
 const main = async () => {
 
     const productsURL = 'https://www.thisiswhyimbroke.com/api/lists/new'
     let counter = 1
     let next = true
-    let products = []
+
+    let amazon = []
+    let etsy = []
+    let kickstarter = []
+    let others = []
 
     while(next){
         const content = await axios.get(`${productsURL}/${counter}`)
@@ -26,12 +34,8 @@ const main = async () => {
                 next = false
             }
 
-            if(post.link.indexOf('amazon') == -1){
-                continue
-            }
-
-            let product = {
-                id: post.link.split('dp/')[1].split('/')[0],
+            const product = {
+                id: shortid.generate(),
                 titleCN: '',
                 titleEN: post.title,
                 description: '',
@@ -42,17 +46,37 @@ const main = async () => {
                 link: '',
                 uri: `https://www.thisiswhyimbroke.com${post.uri}`,
                 source: 'thisiswhyimbroke',
-                exId: post.id
+                exId: post.id,
+                exLink: post.link
             }
-            products.push(product)
+
+            if(post.link.indexOf('amazon') != -1){
+                amazon.push(product)
+            }else if(post.link.indexOf('awin1') != -1){
+                etsy.push(product)
+            }else if(post.link.indexOf('kickstarter') != -1 || post.link.indexOf('kckb') != -1 || post.link.indexOf('indiegogo') != -1){
+                kickstarter.push(product)
+            }else{
+                others.push(product)
+            }
+
         }
 
         counter++
     }
 
-    // write to file
-    const json = JSON.stringify(products, undefined, 2)
-    fs.writeFileSync(filepath, json, 'utf8')  
+    // write to files
+    const amazonjson = JSON.stringify(amazon, undefined, 2)
+    fs.writeFileSync(amazonfilepath, amazonjson, 'utf8')  
+
+    const etsyjson = JSON.stringify(etsy, undefined, 2)
+    fs.writeFileSync(etsyfilepath, etsyjson, 'utf8') 
+
+    const kickstarterjson = JSON.stringify(kickstarter, undefined, 2)
+    fs.writeFileSync(kickstarterfilepath, kickstarterjson, 'utf8') 
+
+    const othersjson = JSON.stringify(others, undefined, 2)
+    fs.writeFileSync(othersfilepath, othersjson, 'utf8') 
 }
 
 main()
