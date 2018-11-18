eac880ea1bb5d8197cb41f8d9e20bec16997b6f3
Jacky Zhao <liang.zhao83@gmail.com>
2018-11-18 15:21:17 -0800

    working on myfavs

--- src/Components/MyFavs/index.js
+++ src/Components/MyFavs/index.js
@@ -1,36 +1,56 @@
 import React, { Component } from 'react'
 import {
-    Grid, Button, CircularProgress
+    Grid, Paper, Tabs, Tab,
 } from '@material-ui/core'
 import { orange, red } from '@material-ui/core/colors'
 import { compose } from 'recompose'
 import { withStyles } from '@material-ui/core/styles'
-import { withContext } from '../../context'
 import Products from '../Products'
 
 
 const styles = theme => ({
-    content: {
-        marginBottom: 20,
+    root: {
+        flexGrow: 1,
     },
 })
 
   
 class MyFavs extends Component {
 
+    state = {
+        value: 0,
+    }
+
+    handleChange = (event, value) => {
+        this.setState({ value });
+    }
+
     render() {
         const { classes } = this.props
+        const { value } = this.state
         
         let products = <Products category={'myfavs'} subcategory={'dummyvalue'} /> 
         let articles = null
 
+        console.log('value ',value)
         return <Grid container justify="center" alignItems="center">
-                    {products}
+                    <Paper className={classes.root}>
+                        <Tabs
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        centered
+                        >
+                        <Tab label="Item One" />
+                        <Tab label="Item Two" />
+                        <Tab label="Item Three" />
+                        </Tabs>
+                    </Paper>
                 </Grid>
     }
 }
 
 export default compose(
-    withContext,
     withStyles(styles)
 )(MyFavs)
